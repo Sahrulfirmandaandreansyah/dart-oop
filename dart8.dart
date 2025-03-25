ce642afbe6fdb5119fa719ec3c7bbabdcb45bac1
unknown <email@example.com>
2025-03-25 12:37:51 +0700

    DART-OOP

---
+++ dart8.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main() {
+  print('Masukkan sebuah angka: ');
+  int angka = int.parse(stdin.readLineSync()!);
+
+  int jumlahDigit = 0;
+  int temp = angka;
+
+  while (temp != 0) {
+    jumlahDigit++;
+    temp ~/= 10; // Operasi pembagian bulat
+  }
+
+  print('Jumlah digit dari $angka adalah $jumlahDigit');
+}
